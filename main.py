5845c25c8d5319577fe659db7c35e470d60061a3
nicetea9 <162029704+nicetea9@users.noreply.github.com>
2024-04-17 18:16:21 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,101 +1,83 @@
-import threading
-import time
-import subprocess
-import streamlink
-import json
-import requests
-import os
-from speechkit import configure_credentials,creds
-from speechkit.stt import AudioProcessingType
-from speechkit import model_repository
-from clearml import Task
-from langchain.chat_models.gigachat import GigaChat
-from langchain.schema import HumanMessage,SystemMessage
-from clearml import Logger
+import streamlit as st
 
-if __name__ == "__main__":
-    task = Task.init(project_name='RTTS', task_name='experiment')
-    all_result=[]
-    url=input("Enter your stream: ")
-    streams=streamlink.streams(url)
-    audio_stream=streams["audio_only"]
-    configure_credentials(
-        yandex_credentials=creds.YandexCredentials(api_key='AQVN3qwuRDroqa-9P2bAlSWuh_UVmVe_CiOHmEdF')
-    )
-    model = model_repository.recognition_model()
-    model.model = 'general:rc'
-    model.language = 'ru-RU'
-    n="c"
-    model.audio_processing_type = AudioProcessingType.Full
-    for i in range(3):
-        duration = 30
-        start=i*duration
-        output=f"fragmen({n})_{i+1}.mp3"
-        ffmpeg = [
-            "ffmpeg",
-            "-i", audio_stream.url,
-            "-ss", str(start),
-            "-t",str(duration),
-            "-vn",
-            "-c","copy",
-            "-acodec", "libmp3lame",
-            output
-        ]
-        subprocess.run(ffmpeg)
-        time.sleep(2)
-        """
-        audio_url = task.upload_artifact(output,name='custom_audio')
-        task.get_logger().report_media(
-            'audio', output, iteration=1,
-            url=audio_url
-        )
-        """
-        audio_artifact=task.upload_artifact(name='Custom_audio',artifact_object=output)
+# Заголовок приложения
 
-        task.get_logger().report_media(
-            title='audio',
-            series='tada',
-            iteration=1,
-            local_path=output
-        )
-        result=model.transcribe_file(output)
-        all_result.append((result[0].normalized_text))
-        print(f"Text {output}: {result[0].normalized_text}")
+st.markdown("<h1 style='text-align: center; color: blue;'>RTTS</h1>", unsafe_allow_html=True)
 
-        """os.remove(f"fragmen({n})_{i+1}.mp3")"""
-
-    all_result_str = '\n'.join(all_result)
-
-    print("Весь текст:", all_result_str)
-
-    client_id = "1a8c7831-f59c-4904-9455-608bc1a3f2af"
-    client_secret = "0a6fc530-79bd-4497-b37f-bf2cc10e5c97"
-    authorization_data = "MWE4Yzc4MzEtZjU5Yy00OTA0LTk0NTUtNjA4YmMxYTNmMmFmOjBhNmZjNTMwLTc5YmQtNDQ5Ny1iMzdmLWJmMmNjMTBlNWM5Nw=="
-
-    chat = GigaChat(credentials=authorization_data,verify_ssl_certs=False, scope="GIGACHAT_API_PERS")
-
-    messages=[
-        SystemMessage(
-            content="Сократи текст:"
-        )
-    ]
-    messages.append(HumanMessage(content=all_result_str))
-    res=chat(messages)
-    messages.append(res)
-    short=open("short.txt","w+")
-    short.write(res.content)
-    print(res.content)
+st.sidebar.info(
     """
-    text_url = task.upload_artifact("short.txt")
-    task.get_logger().report_media(
-        'text', "short", iteration=1,
-        url=text_url
-    )"""
-    text_artifact=task.upload_artifact(name='custom_text',artifact_object='short.txt')
-    task.get_logger().report_media(
-        title='text',
-        series='documents',
-        iteration=1,
-        local_path='short.txt'
-    )
-    task.close()
+    Выберите платформу
+    """
+)
+
+st.sidebar.markdown(f'''
+<a><button style="background-color:lightblue;margin-right: 50px;">Twitch</button></a>
+<a><button style="background-color:lightblue;margin-left: 70px;">YouTube</button></a>
+''', unsafe_allow_html=True)
+
+
+# Используем Markdown для вставки стилей CSS
+st.sidebar.write("""
+    <style>
+    
+   body {
+        background-color:  ightblue !important; /* Цвет фона не меняется почему-то */
+        # /* background-image: url('https:/'); Изображение фона можно поставить 
+        # background-size: cover; /* Масштабируем изображение до размера контейнера */
+        # background-repeat: no-repeat; /* Запрещаем повторение изображения  */
+    }
+
+    .container {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        
+    }
+  .rectangle {
+        width: 700px;
+        height: 550px;
+        border: 1px solid #ccc;  /* Устанавливаем черные границы */
+        background-color: transparent;  /* Прозрачный фон */
+        margin-bottom: 20px;  /* Добавляем отступ между прямоугольниками */
+        margin-left:-400px;
+    }
+    .rectangle2 {
+        width: 700px;
+        height: 550px;
+        border: 1px solid #ccc;  /* Устанавливаем черные границы */
+        background-color: transparent;  /* Прозрачный фон */
+        margin-left:400px;
+        margin-top:-570px;  
+    }
+  
+   
+  
+</style>
+""", unsafe_allow_html=True)
+
+
+# Размещаем кнопку и текстовое поле внутри контейнера
+with st.sidebar.container():
+    # Текстовое поле для ввода URL
+    url = st.text_input('Вставьте URL', key="text_input", value="")
+
+    # Кнопка "Отправить"
+    submitted = st.button('Отправить')
+
+
+st.sidebar.info(
+    """
+    Не знаю можно написать какую-то хрень, типо как пользоваться приложением и тд :)
+    """
+)
+
+# Размещаем окна
+st.markdown('<div class="container">', unsafe_allow_html=True)
+st.markdown('<div class="rectangle"></div>', unsafe_allow_html=True)
+st.markdown('<div class="rectangle2"></div>', unsafe_allow_html=True)
+
+st.markdown('</div>', unsafe_allow_html=True)
+
+
+
+
